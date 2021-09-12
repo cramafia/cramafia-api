88c642bae762233b5343138154db7ce5edab662d
6heLL66 <sseniknik@gmail.com>
2021-09-13 00:46:35 +0300

    fix: rename file and variable

--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -6,11 +6,11 @@ const { Router } = require('express')
 const { register } = require('./registration/register')
 const { isUsernameExist } = require('./registration/isUsernameExist')
 const { routes } = require('../../app-constants/routes')
-const { registerValidations } = require('../../utils/validations')
+const { registerValidators } = require('../../utils/validators')
 
 const authRouter = Router()
 
-authRouter.post(routes.auth.registr, ...registerValidations, isUserExist, register)
+authRouter.post(routes.auth.registr, ...registerValidators, isUserExist, register)
 authRouter.post(routes.auth.isUserExist, isUserExist, isUsernameExist)
 authRouter.post(routes.auth.authenticate, authenticate)
 
--- utils/validators.js
+++ utils/validators.js
@@ -1,10 +1,10 @@
 const { body } = require('express-validator')
 
-const registerValidations = [
+const registerValidators = [
     body('username').isLength({ min: 4, max: 16 }),
     body('password').isLength({ min: 8, max: 32 }),
 ]
 
 module.exports = {
-    registerValidations,
+    registerValidators,
 }
