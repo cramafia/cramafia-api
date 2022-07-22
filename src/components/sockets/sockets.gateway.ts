7c4ff052f5c0143e68c543e66baf0214d3a82cac
n.semenchuk <n.semenchuk@itransition.com>
2022-07-22 15:27:40 +0300

    add-players-count

--- src/components/sockets/sockets.gateway.ts
+++ src/components/sockets/sockets.gateway.ts
@@ -5,7 +5,9 @@ import {
 } from '@nestjs/websockets'
 import { Server, Socket } from 'socket.io'
 import { CreateLobbyDto } from '../lobbies/dto/create-lobby.dto'
+import { UpdateLobbyDto } from '../lobbies/dto/update-lobby-dto'
 import { LobbiesService } from '../lobbies/lobbies.service'
+import { UserDto } from '../users/dto/user.dto'
 
 @WebSocketGateway({
   cors: {
@@ -23,9 +25,9 @@ export class SocketsGateway {
     this.server.emit('getLiveUsersToClient', allActiveSockets)
   }
 
-  async emitGetAllLobbies(client: Socket) {
+  async emitGetAllLobbies() {
     const allLobbies = await this.lobbiesService.getAll()
-    client.emit('getAllLobbies', allLobbies)
+    this.server.emit('getAllLobbies', allLobbies)
   }
 
   clearSockets() {
@@ -50,12 +52,42 @@ export class SocketsGateway {
 
   @SubscribeMessage('getAllLobbies')
   async handleGetAllLobbies(client: Socket) {
-    this.emitGetAllLobbies(client)
+    this.emitGetAllLobbies()
   }
 
   @SubscribeMessage('createLobby')
   async handleCreateLobby(client: Socket, data: CreateLobbyDto) {
     await this.lobbiesService.createLobby(data)
-    this.emitGetAllLobbies(client)
+    this.emitGetAllLobbies()
+  }
+
+  @SubscribeMessage('connectPlayerToLobby')
+  async handleConnectPlayerToLobby(
+    client: Socket,
+    {
+      lobbyId,
+      player,
+    }: {
+      lobbyId: string
+      player: UserDto
+    }
+  ) {
+    await this.lobbiesService.connectPlayerToLobby(lobbyId, player)
+    this.emitGetAllLobbies()
+  }
+
+  @SubscribeMessage('disconnectPlayerFromLobby')
+  async handleDisconnectPlayerFromLobby(
+    client: Socket,
+    {
+      lobbyId,
+      player,
+    }: {
+      lobbyId: string
+      player: UserDto
+    }
+  ) {
+    await this.lobbiesService.disconnectPlayerFromLobby(lobbyId, player)
+    this.emitGetAllLobbies()
   }
 }
